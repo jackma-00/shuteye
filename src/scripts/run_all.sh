06b4e47990bb47feee7ee676ce71db8424abd2bd
jackma-00 <jacopo.maragna1@gmail.com>
2025-09-27 09:16:13 +0200

    Refactor workflow to use run_bot.sh script and add logging functionality

--- src/scripts/run_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# run_all.sh
-# Usage: ./src/scripts/run_all.sh
-# Make executable: chmod +x ./src/scripts/run_all.sh
-
-python -m src.messaging.bot
---
+++ src/scripts/run_bot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# run_bot.sh
+# Usage: ./src/scripts/run_bot.sh
+# Make executable: chmod +x ./src/scripts/run_bot.sh
+
+python -m src.messaging.bot
